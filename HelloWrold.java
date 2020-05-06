4a1ee82db48c69895f7a00b8c06a96b96dd50933
wosli <2097536791@qq.com>
2020-05-06 19:42:32 +0800

    hellowrold

---
+++ HelloWrold.java
@@ -0,0 +1,8 @@
+public class HelloWrold{
+	public static void main(String[] args){
+		system.out.println("HelloWrold");
+		system.out.println("1");
+		system.out.println("HelloWrold");
+		system.out.println("2");
+	}
+}
